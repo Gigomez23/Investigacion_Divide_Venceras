7c22078e999fd71e0d980301d1ec016111dc5357
IGChBl <ichavarria@uamv.edu.ni>
2024-10-10 22:58:18 -0600

    Ejercicio práctico con QuickSort

--- EjercicioPráctico/Ejercicio/Properties/AssemblyInfo.cs
+++ EjercicioPráctico/Ejercicio/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DivideVenceras")]
+[assembly: AssemblyTitle("Ejercicio")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DivideVenceras")]
+[assembly: AssemblyProduct("Ejercicio")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d4dd26ca-ef4d-4648-a8b2-b59fd06418d1")]
+[assembly: Guid("9cb9009f-8c27-4032-8624-6ca39c36b4bb")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ EjercicioPráctico/Ejercicio/models/DivideAndConquerSolver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio.models
+{
+    public class DivideAndConquerSolver
+    {
+        // Método principal para QuickSort
+        public void QuickSort(int[] array)
+        {
+            QuickSortRecursive(array, 0, array.Length - 1);
+        }
+
+        // Método recursivo de QuickSort
+        private void QuickSortRecursive(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = Partition(array, low, high);
+                QuickSortRecursive(array, low, pivotIndex - 1);
+                QuickSortRecursive(array, pivotIndex + 1, high);
+            }
+        }
+
+        // Método para dividir el array en base al pivote
+        private int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (array[j] <= pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            Swap(array, i + 1, high);
+            return i + 1;
+        }
+
+        // Método para intercambiar elementos en el array
+        private void Swap(int[] array, int a, int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+    }
+}
